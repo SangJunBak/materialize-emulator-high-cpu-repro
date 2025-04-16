e7c7173eee5be08b8715fbc88161173b2ecc51d8
Sang Jun Bak <sangjun.bak99@gmail.com>
2025-04-16 15:34:13 -0400

    Add a lot of sources

--- postgres/postgres_bootstrap.sql
+++ postgres/postgres_bootstrap.sql
@@ -1,19 +1,46 @@
-ALTER SYSTEM SET wal_level = logical;
-ALTER ROLE postgres WITH REPLICATION;
-
 CREATE SCHEMA ecommerce;
-
-CREATE TABLE "ecommerce"."products" (
-  "id" int PRIMARY KEY,
-  "username" varchar,
-  "email" varchar,
-  "password" varchar,
-  "age" int,
-  "status" int
+-- Create the ecommerce schema if it doesn't exist
+CREATE SCHEMA IF NOT EXISTS "ecommerce";
+DO $$
+DECLARE i INT;
+table_name TEXT;
+BEGIN -- Loop 2000 times
+FOR i IN 1..2000 LOOP -- Create dynamic table name
+table_name := 'products_' || i;
+-- Create table
+EXECUTE format(
+  '
+            CREATE TABLE "ecommerce"."%s" (
+                "id" int PRIMARY KEY,
+                "username" varchar,
+                "email" varchar,
+                "password" varchar,
+                "age" int,
+                "status" int
+            )',
+  table_name
 );
-
--- Enable REPLICA for both tables
-ALTER TABLE "ecommerce"."products" REPLICA IDENTITY FULL;
-
--- Create publication on the created tables
-CREATE PUBLICATION mz_source FOR TABLE "ecommerce"."products";
+-- Set REPLICA IDENTITY
+EXECUTE format(
+  'ALTER TABLE "ecommerce"."%s" REPLICA IDENTITY FULL',
+  table_name
+);
+-- Insert one row with the table number as the ID
+EXECUTE format(
+  '
+            INSERT INTO "ecommerce"."%s" 
+            VALUES (%s, ''user_%s'', ''user_%s@example.com'', ''pass_%s'', 25, 1)',
+  table_name,
+  i,
+  i,
+  i,
+  i
+);
+END LOOP;
+END $$;
+ALTER SYSTEM
+SET wal_level = logical;
+ALTER ROLE postgres WITH REPLICATION;
+-- Update the publication to include all tables
+DROP PUBLICATION IF EXISTS mz_source;
+CREATE PUBLICATION mz_source FOR ALL TABLES;
